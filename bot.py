3799062bc32e1a4fd8299036416fad93aba0cce0
Luca <Codfish246@users.noreply.github.com>
2020-05-21 01:15:01 +0100

    Update bot.py with first and main command
    Add main word finding command to bot including function it needs

--- bot.py
+++ bot.py
@@ -23,11 +23,39 @@ async def on_ready():
     await bot.change_presence(activity=activity)
 
 
+def findWholeWord(w):
+    return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
 
+
+    if message.content == ('!find'):
+        channel = message.channel
+
+        await channel.send('Send the word you wish to find:')
+        def check(m):
+            return m.channel == channel and m.author == message.author
+        wordToFind = await bot.wait_for('message', check=check)
+
+        await channel.send('Send the text you wish to search:')
+        def check(m):
+            return m.author == message.author and m.channel == channel
+        textToSearch = await bot.wait_for('message', check=check)
+
+        findWordFunct = findWholeWord(wordToFind.content)(textToSearch.content)
+        
+        emoji = '\N{THUMBS UP SIGN}'
+        await textToSearch.add_reaction(emoji)
+        
+        if (findWordFunct is not None):
+            await channel.send('Word ' + wordToFind.content + ' found in text, at text character span: ' +  str(findWordFunct.span()))
+        else:
+            await channel.send('Word ' + wordToFind.content + ' not found in text.')
+
+        #findWholeWord('seek')('those who seek shall find') #match
 
 
 bot.run(TEST_TOKEN) #fishybot for testing 
